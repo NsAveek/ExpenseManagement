d7f0d3117b3fef91a37ef6e0b6df083b80ed253a
Aveek <nsaveek@gmail.com>
2019-04-07 19:54:21 +0800

    Integrated Room database inside the project

---
+++ app/src/main/java/aveek/com/management/ui/db/dao/IncomeDAO.kt
@@ -0,0 +1,7 @@
+package aveek.com.management.ui.db.dao
+
+import android.arch.persistence.room.Dao
+
+@Dao
+interface IncomeDAO  {
+}
---
+++ app/src/main/java/aveek/com/management/ui/db/entity/Income.kt
@@ -0,0 +1,7 @@
+package aveek.com.management.ui.db.entity
+
+import android.arch.persistence.room.Entity
+
+@Entity
+class Income {
+}

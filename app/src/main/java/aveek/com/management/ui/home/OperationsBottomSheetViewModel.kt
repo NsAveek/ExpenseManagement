e0a4844aab1183577dc279a75ccbe8382ab2e828
Aveek <Aveek@kl-pt-009.icarasia.local>
2019-05-16 14:47:41 +0800

    Updated gradle and two way databinding

--- app/src/main/java/aveek/com/management/ui/home/OperationsBottomSheetViewModel.kt
+++ app/src/main/java/aveek/com/management/ui/home/OperationsBottomSheetViewModel.kt
@@ -2,11 +2,17 @@ package aveek.com.management.ui.home
 
 import android.arch.lifecycle.MutableLiveData
 import android.arch.lifecycle.ViewModel
+import android.databinding.ObservableField
+import android.databinding.ObservableInt
+import aveek.com.management.ui.db.entity.Transaction
 
 class OperationsBottomSheetViewModel : ViewModel() {
 
     val addData = MutableLiveData<Boolean>().apply { value = false }
     val dismissData = MutableLiveData<Boolean>().apply { value = false }
+    val amount = ObservableField<String>().apply { set("") }
+    val purpose = ObservableField<String>().apply { set("") }
+    val date = ObservableField<String>().apply { set("") }
 
     fun add(){
         // TODO : add data from the user input and pass to the fragment
@@ -17,7 +23,10 @@ class OperationsBottomSheetViewModel : ViewModel() {
         addData.value = true
         dismissData.value = false
 
-
+        val checkamount = amount.get()
+        val checkpurpose = purpose.get()
+        val checkdate = date.get()
+        val coll = "$checkamount $checkpurpose $checkdate "
     }
     fun dismissBottomSheet(){
         addData.value = false
